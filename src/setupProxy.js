94a6e0b20409b0b5b64cedd92e9ce77cee23666a
Matthew <mmilideo@gmail.com>
2019-04-22 22:08:20 -0400

    Added the server and recovered from poor commit

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require('http-proxy-middleware')
-
-module.exports = function(app) {
-    app.use(proxy('/games', { target: 'https://api-v3.igdb.com', changeOrigin: true }));
-  };
-  
