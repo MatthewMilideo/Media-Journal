245b39efd6e600fb10eb70e840b3e3344cded4ab
Matthew <mmilideo@gmail.com>
2019-04-22 23:22:38 -0400

    added procfile

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
 //Static file declaration
-const express = require("express");
-const jsonServer = require("json-server");
-const middlewares = jsonServer.defaults();
+const express = require('express');
+const jsonServer = require('json-server');
+const middlewares = jsonServer.defaults()
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(middlewares);
-app.use("/api", jsonServer.router("db.json"));
+app.use('/api', jsonServer.router('db.json'));
 
 //production mode
-
-app.use(express.static(path.join(__dirname, "client/build")));
-//
-app.get("*", (req, res) => {
-	res.sendfile(path.join((__dirname = "client/build/index.html")));
-});
-
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
 //build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
 //start server
 app.listen(port, (req, res) => {
-	console.log(`server listening on port: ${port}`);
-});
+console.log( `server listening on port: ${port}`);
+})
+
+
+
+
+
+
+
 
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
-// server.use('/api', jsonServer.defaults());
+// server.use('/api', jsonServer.defaults()); 
+
+
+
