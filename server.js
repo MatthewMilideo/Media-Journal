b1ac1b51ffc9b78005b9b663ffaa20551c77cf1b
Matthew <mmilideo@gmail.com>
2019-04-23 20:23:14 -0400

    Turned off CORS as a test.

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 //Static file declaration
 const express = require('express');
 const jsonServer = require('json-server');
-const middlewares = jsonServer.defaults([''])
+const middlewares = jsonServer.defaults();
 const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 
+var cors = require('cors')
+
+app.use(cors())
 
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
 // server.use('/api', jsonServer.defaults()); 
 
 
-
+const router = jsonServer.router('db.json');
+app.use('/api', router);
 
 //production mode
 if(process.env.NODE_ENV === 'production') {
@@ -24,16 +28,12 @@ if(process.env.NODE_ENV === 'production') {
     res.sendfile(path.join(__dirname, 'client/build/index.html'));
   })
 }
-let x = path.join(__dirname,'/client/build'); 
-console.log(x);
-
 //build mode
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/public/index.html'));
 })
 
-const router = jsonServer.router('db.json');
-app.use('/api', router);
+
 
 console.log('test');
 //start server
