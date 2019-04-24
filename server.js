24b6b5428fc1156079fdb60d68294ee1a3307f26
Matthew <mmilideo@gmail.com>
2019-04-23 21:42:10 -0400

    trying new cors setup

--- server.js
+++ server.js
@@ -1,63 +1,43 @@
 //Static file declaration
-const express = require('express');
-const jsonServer = require('json-server');
+const express = require("express");
+const jsonServer = require("json-server");
 const middlewares = jsonServer.defaults();
-const path = require('path')
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+var cors = require("cors");
 
-var cors = require('cors')
+app.use(
+	cors({
+		origin: process.env.PUBLIC_URL
+	})
+);
 
-app.use(cors());
 //app.use(middlewares);
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 router.use(middlewares);
-app.use('/api', router);
+app.use("/api", router);
 
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
-// server.use('/api', jsonServer.defaults()); 
-
+// server.use('/api', jsonServer.defaults());
 
 //production mode
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/client/build")));
 
- 
-  
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname, 'client/build/index.html'));
-  })
+	app.get("*", (req, res) => {
+		res.sendfile(path.join(__dirname, "client/build/index.html"));
+	});
 }
 
+//build mode
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/client/public/index.html"));
+});
 
-
-
-
-  //build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/public/index.html'));
-})
-
-
-
-
-
-console.log('test');
+console.log("test");
 //start server
-app.listen(port, (req, res) => {
-})
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(port, (req, res) => {});
