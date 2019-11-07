6c20f3f62f63e4ab5c1949040a5f907a28e5f783
Matthew <mmilideo@gmail.com>
2019-11-07 10:14:52 -0500

    First Branch Commit

--- routes/users_route.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express-promise-router");
-
-const NotesController = require("../controllers/notes_cont");
-
-const router = new Router();
-
-router.post("/", NotesController.postUser);
-
-module.exports = router;
--- types.js
+++
@@ -1,8 +0,0 @@
-const MOVIE = 'MOVIE';
-const TV = 'TV'; 
-const BOOK = 'BOOK'; 
-const GAME = 'GAME'; 
-
-module.exports = {
-    MOVIE, TV, BOOK, GAME
-  }
